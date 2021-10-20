fd2d6a05688fdd5a51fc3eeab9ffac04d0fddcef
Jenny Bykova <eva.gre4ka@yandex.ru>
2021-10-20 21:34:18 +0300

    Add bubble sort

---
+++ src/BubbleSort.js
@@ -0,0 +1,19 @@
+let arr = [4, 5, 1, 3, 6, 2];
+
+let bubbleSort = (arr) => {
+    let lengthArr = arr.length;
+    for (let i = 0; i < lengthArr - 1; i++) {
+        for (let k = 0; k < lengthArr - i - 1; k++){
+            if (arr[k] > arr[k + 1]){
+                let temp = arr[k];
+                arr[k] = arr[k + 1];
+                arr[k + 1] = temp;
+            }
+        }
+    }
+    return arr;
+}
+
+
+console.log(arr);
+console.log(bubbleSort(arr));
